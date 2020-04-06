26f695c41c0d66a467a81f98b661dbec70b50c34
Liu Pengpeng <liupppppp@gmail.com>
2020-04-06 17:15:42 +0800

    Update ByteUtils.java

--- src/main/java/io/eblock/eos4j/utils/ByteUtils.java
+++ src/main/java/io/eblock/eos4j/utils/ByteUtils.java
@@ -383,13 +383,13 @@ public class ByteUtils {
 	 */
 	public static byte[] writerKey(String key) {
 		io.eblock.eos4j.utils.ByteBuffer bf = new io.eblock.eos4j.utils.ByteBuffer();
-		bf.concat(writerUnit32("1"));
-		bf.concat(writerVarint32("1"));
-		bf.concat(writerVarint32("0"));
+// 		bf.concat(writerUnit32("1"));
+// 		bf.concat(writerVarint32("1"));
+// 		bf.concat(writerVarint32("0"));
 		bf.concat(writerKeyStr(key));
-		bf.concat(writerUnit16("1"));
-		bf.concat(writerVarint32("0"));
-		bf.concat(writerVarint32("0"));
+// 		bf.concat(writerUnit16("1"));
+// 		bf.concat(writerVarint32("0"));
+// 		bf.concat(writerVarint32("0"));
 		return bf.getBuffer();
 	}
 	
